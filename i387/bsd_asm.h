7e509570e3f163bbad2943c13e645c4a52c7f151
Igor Popov <popov.nirvana@gmail.com>
2025-08-14 02:44:00 +0300

    Fix build

--- i387/bsd_asm.h
+++ i387/bsd_asm.h
@@ -78,16 +78,20 @@
 #define _ENTRY(x) .text; _START_ENTRY; \
 			.globl CNAME(x); .type CNAME(x),@function; CNAME(x):
 #define	END(x)		.size x, . - x
-#elif defined(_WIN32)
-#ifndef _MSC_VER
+
+#else // #elif defined(_WIN32)
+
+//#ifndef _MSC_VER
 #define END(x) .end
 #define _START_ENTRY_WIN .text; _START_ENTRY
-#else
-#define END(x) end
-#define _START_ENTRY_WIN .code; _START_ENTRY
-#endif
+//#else
+//#define END(x) end
+//#define _START_ENTRY_WIN .code; _START_ENTRY
+//#endif
+
 #define CNAME(csym)		_##csym
 #define HIDENAME(asmsym)	.asmsym
+
 #define _ENTRY(x) _START_ENTRY_WIN; \
             .globl CNAME(x); .section .drectve; .ascii " -export:", #x; \
             .section .text; .def CNAME(x); .scl 2; .type 32; .endef; CNAME(x):
